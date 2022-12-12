8a7f53c8a899025f982bf2fb9324c6f1b8c195be
KeeganM22 <119663094+KeeganM22@users.noreply.github.com>
2022-12-12 02:01:01 -0700

    updated to facility final

--- classes/fac.py
+++ classes/fac.py
@@ -19,7 +19,13 @@ class facilities:
         for x in facilitiesList:
             print(x)
 
-test = facilities(1)
-test.addFacility()
-test.writeListOffacilitiesToFile()
-test.displayFacilities()
+test = facilities("test")
+inp = 1
+while int(inp) < 6:
+    print("facilities menu \n 1 - Display facilities list \n 2 - add facility \n 3 - back to main menu")
+    inp = input()
+    if int(inp) == 1:
+        test.displayFacilities()
+    elif int(inp) == 2:
+        test.addFacility()
+        test.writeListOffacilitiesToFile()
